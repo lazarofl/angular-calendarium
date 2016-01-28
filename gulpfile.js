8ee347a6927503bdc6b970634bec56c94a56ba87
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2016-01-28 18:13:01 -0200

    css minify on gulp task, gulp rename;util and minify - default css used in example page

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,40 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	clean = require('gulp-contrib-clean'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css'),
+	rename = require("gulp-rename");
 
 // build the main source into the min file 
-gulp.task('default', ['clean'], function () {
+gulp.task('javascript', function () {
     return gulp.src(['src/angular-calendarium.js'])
     			.pipe(jshint())
     			.pipe(jshint.reporter('jshint-stylish'))
     			.pipe(jshint.reporter('fail'))
+    			.pipe(gulp.dest('dist/js'))
     			.pipe(uglify())
-    			.pipe(gulp.dest('dist/js'));
+			    .pipe(rename(function(path) {
+			        path.basename += '.min';
+			    }))
+			    .pipe(gulp.dest('dist/js'));
+});
+
+// build the css
+gulp.task('styles', function () {
+    return gulp.src(['css/angular-calendarium.css'])
+    			.pipe(gulp.dest('dist/css'))
+    			.pipe(minifyCss({compatibility: 'ie8'}))
+    			.pipe(rename(function(path) {
+			        path.basename += '.min';
+			    }))
+    			.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('clean', function() {
 	return gulp.src('dist')
 		.pipe(clean());
+});
+
+gulp.task('default', ['clean', 'javascript', 'styles'], function() {
+	
 });
